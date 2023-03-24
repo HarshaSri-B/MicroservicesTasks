75c700ad3e1ebc0e8c3f3732ce6dae8e25f1781a
Harsha Sri Bandi <harsha_bandi@persistent.com>
2023-03-24 11:26:52 +0530

    POC for api-gateway & circuit breaker

---
+++ MicroservicesTask1/currency-exchange-service/src/main/java/com/example/demo/CurrencyExchangeController.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyExchangeController {
+
+	@Autowired
+	private Environment environment;
+
+	@Autowired
+	private CurrencyExchangeRepository repo;
+
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyExchange retriveExchangeValue(@PathVariable String from, @PathVariable String to) {
+
+		CurrencyExchange ce = repo.findByFromAndTo(from, to);
+
+		if (ce == null) {
+			throw new RuntimeException("Unable to find data for " + from + " to " + to);
+		}
+
+		// CurrencyExchange ce = new CurrencyExchange(1000L, from, to,
+		// BigDecimal.valueOf(50));
+		String port = environment.getProperty("local.server.port");
+		ce.setEnvironment(port);
+		return ce;
+	}
+
+}
---
+++ MicroservicesTask1/currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,20 @@
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(1001,'USD','INR',70,'');
+
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(1002,'EUR','INR',75,'');
+
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(1003,'AED','INR',23,'');
+
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(1004,'YUMA','INR',12,'');
+
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(1005,'AUD','INR',25,'');
+
